4ffd6fc0ad713fb6b4a5eddf0314c07042dffc9f
Andy Liang <andyzen619@gmail.com>
2019-11-25 14:19:27 -0500

    Finished implementing DayListItem with format spots, all test passing

---
+++ src/components/DayList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import cs from "classnames";
+
+import "components/DayListItem.scss"
+
+export default function(props) {
+
+  return (
+    <ul>
+      
+    </ul>
+  )
+
+}
